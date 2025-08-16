421fc945bc713b02be2e126a8594d9a507a9dddd
nobodyisherecu <nobodyisherecu@gmail.com>
2025-08-16 18:02:03 +0000

    changed lpack behavior to dont return None as that can introduce more bugs than sending the base string (better if you send the english one in future)

--- library/lpak.py
+++ library/lpak.py
@@ -1,13 +1,13 @@
-def get(word, language):
-    
+def get(word, language)-> str:
+
     line_found = []
 
-    with open(f"lpak/{language}.lpak", "r") as file:        
+    with open(f"lpak/{language}.lpak", "r") as file:
         for line in file:
-                parts = line.split("|", 1)              
-                
-                key, value = parts[0].strip(), parts[1].strip()                
-              
+                parts = line.split("|", 1)
+
+                key, value = parts[0].strip(), parts[1].strip()
+
                 if key == word:
-                    return str(value)                
-    return None
+                    return str(value)
+    return word # if there no lang return some, dont make an error by this one
