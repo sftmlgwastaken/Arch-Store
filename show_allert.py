39e2be8ec4045ed296182bcaa92ad508043eb4ec
Samuobe <samuobe@ik.me>
2025-08-23 10:09:49 +0200

    Now there is the icon on the show message

--- show_allert.py
+++ show_allert.py
@@ -1,8 +1,10 @@
 import PyQt6.QtWidgets as pq
+from PyQt6.QtGui import QIcon
 def show_allert(title, message):
     msg = pq.QMessageBox()
     msg.setIcon(pq.QMessageBox.Icon.Information)
     msg.setText(message)
     msg.setWindowTitle(title)
+    msg.setWindowIcon(QIcon("icon.png"))
     msg.setStandardButtons(pq.QMessageBox.StandardButton.Ok | pq.QMessageBox.StandardButton.Cancel)
     retval = msg.exec()
