1008938b7060e5c050a1b333280d2a7b8a219f80
Samuobe <samuobe@ik.me>
2025-08-24 00:48:44 +0200

    Refactor installation script to improve dependency handling and cleanup

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "2) Uninstall Arch-Store"
 read -p "Select an option [1/2]: " action
 
 if [[ "$action" == "1" ]]; then
-    install_dependencies
+    # install_dependencies   # <--- togli se non hai questa funzione
 
     PYTHON_PATH=$(which python3)
     clear
@@ -16,16 +16,19 @@ if [[ "$action" == "1" ]]; then
         echo "Installing Arch-Store stable..."
         wget https://raw.githubusercontent.com/samuobe/Arch-Store/main/PKGBUILD/PKGBUILD
         makepkg -si
-        remove PKGBUILD
+        rm PKGBUILD
         echo "FINISHED!"       
     else
         echo "Installing Arch-Store from main branch..."        
         wget https://raw.githubusercontent.com/samuobe/Arch-Store/main/PKGBUILD/PKGBUILD-git
         mv PKGBUILD-git PKGBUILD
         makepkg -si
+        rm PKGBUILD
         echo "FINISHED!"        
+    fi
 
 elif [[ "$action" == "2" ]]; then
     echo "Uninstalling Arch-Store..."
-    sudo pacman -Rn arch-store
-    sudo pacman -Rn arch-store-git
+    sudo pacman -Rns arch-store
+    sudo pacman -Rns arch-store-git
+fi
